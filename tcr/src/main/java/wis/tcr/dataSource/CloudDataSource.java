4a7f662469fe88aa4040c28753a532ae35233801
wfyue <wfyue0808@gmail.com>
2017-10-14 14:22:53 +0800

    从FTP下载文件

--- tcr/src/main/java/wis/tcr/dataSource/CloudDataSource.java
+++ tcr/src/main/java/wis/tcr/dataSource/CloudDataSource.java
@@ -20,60 +20,13 @@ import org.apache.commons.net.ftp.FTPReply;
 
 public class CloudDataSource {
 
-    public static boolean saveUrlAs(String photoUrl, String fileName) {  
-        //此方法只能用户HTTP协议  
-            try {  
-              URL url = new URL(photoUrl);  
-              HttpURLConnection connection = (HttpURLConnection) url.openConnection();  
-              DataInputStream in = new DataInputStream(connection.getInputStream());  
-              DataOutputStream out = new DataOutputStream(new FileOutputStream(fileName));  
-              byte[] buffer = new byte[4096];  
-              int count = 0;  
-              while ((count = in.read(buffer)) > 0) {  
-                out.write(buffer, 0, count);  
-              }  
-              out.close();  
-              in.close();  
-              return true;  
-            }  
-            catch (Exception e) {  
-              return false;  
-            }  
-          }  
-           
-        public String getDocumentAt(String urlString) {  
-        //此方法兼容HTTP和FTP协议  
-            StringBuffer document = new StringBuffer();  
-            try {  
-              URL url = new URL(urlString);  
-              URLConnection conn = url.openConnection();  
-              BufferedReader reader = new BufferedReader(new InputStreamReader(conn.  
-                  getInputStream()));  
-              String line = null;  
-              while ( (line = reader.readLine()) != null) {  
-                document.append(line + "/n");  
-              }  
-              reader.close();  
-            }  
-            catch (MalformedURLException e) {  
-              System.out.println("Unable to connect to URL: " + urlString);  
-            }  
-            catch (IOException e) {  
-              System.out.println("IOException when connecting to URL: " + urlString);  
-            }  
-            return document.toString();  
-          }  
+ 
           
         public static void main(String[] args) {  
            
+  
               
-             /* String photoUrl = "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png";                                    
-              String fileName = photoUrl.substring(photoUrl.lastIndexOf("/"));  
-              String filePath = "d:/";  
-              boolean flag = saveUrlAs(photoUrl, filePath + fileName);  
-              System.out.println("Run ok!/n<BR>Get URL file " + flag);  */
-              
-        	downFile("10.1.72.41", 21, "pub_nwp", "nafp8nmic", "NAFP/CMA/GRAPES_GFS/20170924/12",null, "d:/aaa");
+        	downFile("10.1.72.41", 21, "pub_nwp", "nafp8nmic", "NAFP/CMA/GRAPES_GFS/20171009/00/",null, "d:/aaa");
         	
         	
         }  
